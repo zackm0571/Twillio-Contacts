2e660eed9f1a94d61191222d539121ed4d5e55da
Zack Matthews <zack@zackmatthews.com>
2017-11-21 10:32:35 -0500

    Makefile working

--- Cpp/src/main.cpp
+++ Cpp/src/main.cpp
@@ -1,5 +1,8 @@
 #include <base-contact-sdk.h>
-
+#include "rapidjson/document.h"
+#include "lib/rapidjson/writer.h"
+#include "lib/rapidjson/stringbuffer.h"
+#include <iostream>
 int main(){
 	printf("Hello world");	
 	return 0;
