da6bd14ac4b56922faba5fc120a98309bf395cf4
Zack Matthews <zack@zackmatthews.com>
2017-11-21 11:00:15 -0500

    Successfully imported rapidjson

--- Cpp/src/main.cpp
+++ Cpp/src/main.cpp
@@ -1,9 +1,26 @@
 #include <base-contact-sdk.h>
 #include "rapidjson/document.h"
-#include "lib/rapidjson/writer.h"
-#include "lib/rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
 #include <iostream>
+using namespace rapidjson;
 int main(){
-	printf("Hello world");	
+    // 1. Parse a JSON string into DOM.
+    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+    Document d;
+    d.Parse(json);
+
+    // 2. Modify it by DOM.
+    Value& s = d["stars"];
+    s.SetInt(s.GetInt() + 1);
+
+    // 3. Stringify the DOM
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    d.Accept(writer);
+
+    // Output {"project":"rapidjson","stars":11}
+    std::cout << buffer.GetString() << std::endl;
+
 	return 0;
 }
