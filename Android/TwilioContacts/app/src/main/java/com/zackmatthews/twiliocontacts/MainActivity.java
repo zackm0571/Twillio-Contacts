a281f287e773914e4028445ef44263ed520fa181
Zack Matthews <zack@zackmatthews.com>
2017-11-26 18:58:30 -0500

    Add contact floating action button

--- Android/TwilioContacts/app/src/main/java/com/zackmatthews/twiliocontacts/MainActivity.java
+++ Android/TwilioContacts/app/src/main/java/com/zackmatthews/twiliocontacts/MainActivity.java
@@ -1,8 +1,15 @@
 package com.zackmatthews.twiliocontacts;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -12,8 +19,16 @@ import com.zackmatthews.twiliocontacts.models.Contact;
 /* Sample app, executes tests by default *
  * set ENABLE_TESTS to false to disable tests */
 public class MainActivity extends Activity{
+    /** Constants **/
     private static final boolean ENABLE_TESTS = true;
+
+    /** UI **/
     private ListView listView;
+    private FloatingActionButton fab;
+    private AlertDialog addContactDialog;
+    private View addContactView;
+
+    /** Misc **/
     private Handler handler = new Handler();
     static {
         System.loadLibrary("native-lib");
@@ -27,6 +42,73 @@ public class MainActivity extends Activity{
         ContactListAdapater listViewAdapter = new ContactListAdapater(MainActivity.this);
         listView = findViewById(R.id.contactListView);
         listView.setAdapter(listViewAdapter);
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(addContactDialog != null){
+                    addContactDialog.dismiss();
+                }
+                if(addContactView == null){
+                    LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+                    if(inflater != null) {
+                        addContactView = inflater.inflate(R.layout.add_contact_menu, (ViewGroup)findViewById(R.id.mainViewGroup), false);
+                    }
+                }
+                addContactDialog = new AlertDialog.Builder(MainActivity.this).setMessage("Add contact")
+                                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                if(addContactView == null) return;
+                                                EditText et_firstName = addContactView.findViewById(R.id.add_Contact_tv_firstName);
+                                                EditText et_lastName = addContactView.findViewById(R.id.add_Contact_tv_lastName);
+                                                EditText et_phoneNumber = addContactView.findViewById(R.id.add_Contact_tv_phoneNumber);
+
+                                                String firstName = et_firstName.getText().toString();
+                                                String lastName = et_lastName.getText().toString();
+                                                String phoneNumber = et_phoneNumber.getText().toString();
+
+                                                boolean isValid =
+                                                        (firstName.replace(" ", "").length() > 0 )
+                                                        && (lastName.replace(" ", "").length() > 0 )
+                                                        && (phoneNumber.replace(" ", "").length() > 0 );
+
+                                                if(isValid) {
+                                                    Contact contact = new Contact(firstName, lastName, phoneNumber);
+                                                    ContactSdk.getInstance().addContact(contact, (ContactListAdapater)listView.getAdapter());
+                                                }
+                                                else{
+                                                    Toast.makeText(MainActivity.this, "Invalid input", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        })
+                                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                dialogInterface.dismiss();
+                                            }
+                                        })
+                                        .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                                            @Override
+                                            public void onDismiss(DialogInterface dialogInterface) {
+                                                EditText et_firstName = addContactView.findViewById(R.id.add_Contact_tv_firstName);
+                                                EditText et_lastName = addContactView.findViewById(R.id.add_Contact_tv_lastName);
+                                                EditText et_phoneNumber = addContactView.findViewById(R.id.add_Contact_tv_phoneNumber);
+
+                                                et_firstName.setText("");
+                                                et_lastName.setText("");
+                                                et_phoneNumber.setText("");
+                                            }
+                                        })
+                                        .setView(addContactView).create();
+
+                if(addContactView.getParent()!=null)
+                    ((ViewGroup)addContactView.getParent()).removeView(addContactView);
+
+                addContactDialog.show();
+            }
+        });
         if(ENABLE_TESTS) {
             addContactTest();
             updateContactTest();
--- Android/TwilioContacts/app/src/main/java/com/zackmatthews/twiliocontacts/models/Contact.java
+++ Android/TwilioContacts/app/src/main/java/com/zackmatthews/twiliocontacts/models/Contact.java
@@ -8,4 +8,13 @@ public class Contact {
     public String firstName;
     public String lastName;
     public String phoneNumber; //E.164
+
+    public Contact(){
+
+    }
+    public Contact(String firstName, String lastName, String phoneNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
 }
