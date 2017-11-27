71a8eba1e8c331fedfc13ba40c3e24f08a01fbc6
Zack Matthews <zack@zackmatthews.com>
2017-11-26 19:56:28 -0500

    Add Javadoc for ContactSDK

--- Android/TwilioContacts/app/src/main/java/com/zackmatthews/twiliocontacts/manager/ContactSdk.java
+++ Android/TwilioContacts/app/src/main/java/com/zackmatthews/twiliocontacts/manager/ContactSdk.java
@@ -4,18 +4,18 @@ import com.zackmatthews.twiliocontacts.models.Contact;
 import java.util.ArrayList;
 
 public class ContactSdk{
-
+	//Load contact lib
 	static {
 		System.loadLibrary("contact-lib");
 	}
-
+	//Singleton
 	public static ContactSdk getInstance() {
 		if(instance == null){
 			instance = new ContactSdk();
 		}
 		return instance;
 	}
-
+	//Callback getter / setter
 	public ContactListener getListener() {
 		return listener;
 	}
@@ -24,15 +24,33 @@ public class ContactSdk{
 		if(listener == null) return;
 		this.listener = listener;
 	}
-
+	//Callback interface
 	public interface ContactListener{
 		void onContactAdded(Contact contact);
 		void onContactUpdated(Contact oldContact, Contact newContact);
 	}
-
+	//Properties
 	private static ContactSdk instance;
 	private ContactListener listener;
+	//NDK wrapper
+
+	/**
+	 * @param contact Contact to add
+	 * @param contactListener Listener to receive callbacks
+	 * @return boolean whether operation was successful
+	 */
 	public native boolean addContact(Contact contact, ContactListener contactListener);
+
+	/**
+	 * @param oldContact Contact to update
+	 * @param newContact Updated contact
+	 * @param contactListener Listener to receive callbacks
+	 * @return boolean whether operation was successful
+	 */
 	public native boolean updateContact(Contact oldContact, Contact newContact, ContactListener contactListener);
+
+	/**
+	 * @return List of native contacts
+	 */
 	public native ArrayList<Contact> getContacts();
 }
