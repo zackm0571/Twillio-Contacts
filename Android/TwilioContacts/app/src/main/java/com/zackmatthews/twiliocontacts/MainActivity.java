7dd286c73eeebfe57dbf0277b1c6e78d5101ac72
Zack Matthews <zack@zackmatthews.com>
2017-11-26 19:00:56 -0500

    Condense init logic

--- Android/TwilioContacts/app/src/main/java/com/zackmatthews/twiliocontacts/MainActivity.java
+++ Android/TwilioContacts/app/src/main/java/com/zackmatthews/twiliocontacts/MainActivity.java
@@ -39,6 +39,15 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initUI();
+
+        if(ENABLE_TESTS) {
+            addContactTest();
+            updateContactTest();
+        }
+    }
+
+    private void initUI(){
         ContactListAdapater listViewAdapter = new ContactListAdapater(MainActivity.this);
         listView = findViewById(R.id.contactListView);
         listView.setAdapter(listViewAdapter);
@@ -47,72 +56,71 @@ public class MainActivity extends Activity{
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(addContactDialog != null){
-                    addContactDialog.dismiss();
-                }
-                if(addContactView == null){
-                    LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
-                    if(inflater != null) {
-                        addContactView = inflater.inflate(R.layout.add_contact_menu, (ViewGroup)findViewById(R.id.mainViewGroup), false);
-                    }
-                }
-                addContactDialog = new AlertDialog.Builder(MainActivity.this).setMessage("Add contact")
-                                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialogInterface, int i) {
-                                                if(addContactView == null) return;
-                                                EditText et_firstName = addContactView.findViewById(R.id.add_Contact_tv_firstName);
-                                                EditText et_lastName = addContactView.findViewById(R.id.add_Contact_tv_lastName);
-                                                EditText et_phoneNumber = addContactView.findViewById(R.id.add_Contact_tv_phoneNumber);
-
-                                                String firstName = et_firstName.getText().toString();
-                                                String lastName = et_lastName.getText().toString();
-                                                String phoneNumber = et_phoneNumber.getText().toString();
-
-                                                boolean isValid =
-                                                        (firstName.replace(" ", "").length() > 0 )
-                                                        && (lastName.replace(" ", "").length() > 0 )
-                                                        && (phoneNumber.replace(" ", "").length() > 0 );
-
-                                                if(isValid) {
-                                                    Contact contact = new Contact(firstName, lastName, phoneNumber);
-                                                    ContactSdk.getInstance().addContact(contact, (ContactListAdapater)listView.getAdapter());
-                                                }
-                                                else{
-                                                    Toast.makeText(MainActivity.this, "Invalid input", Toast.LENGTH_SHORT).show();
-                                                }
-                                            }
-                                        })
-                                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialogInterface, int i) {
-                                                dialogInterface.dismiss();
-                                            }
-                                        })
-                                        .setOnDismissListener(new DialogInterface.OnDismissListener() {
-                                            @Override
-                                            public void onDismiss(DialogInterface dialogInterface) {
-                                                EditText et_firstName = addContactView.findViewById(R.id.add_Contact_tv_firstName);
-                                                EditText et_lastName = addContactView.findViewById(R.id.add_Contact_tv_lastName);
-                                                EditText et_phoneNumber = addContactView.findViewById(R.id.add_Contact_tv_phoneNumber);
-
-                                                et_firstName.setText("");
-                                                et_lastName.setText("");
-                                                et_phoneNumber.setText("");
-                                            }
-                                        })
-                                        .setView(addContactView).create();
-
-                if(addContactView.getParent()!=null)
-                    ((ViewGroup)addContactView.getParent()).removeView(addContactView);
-
-                addContactDialog.show();
+                showAddContactMenu();
             }
         });
-        if(ENABLE_TESTS) {
-            addContactTest();
-            updateContactTest();
+    }
+    private void showAddContactMenu(){
+        if(addContactDialog != null){
+            addContactDialog.dismiss();
         }
+        if(addContactView == null){
+            LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+            if(inflater != null) {
+                addContactView = inflater.inflate(R.layout.add_contact_menu, (ViewGroup)findViewById(R.id.mainViewGroup), false);
+            }
+        }
+        addContactDialog = new AlertDialog.Builder(MainActivity.this).setMessage("Add contact")
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if(addContactView == null) return;
+                        EditText et_firstName = addContactView.findViewById(R.id.add_Contact_tv_firstName);
+                        EditText et_lastName = addContactView.findViewById(R.id.add_Contact_tv_lastName);
+                        EditText et_phoneNumber = addContactView.findViewById(R.id.add_Contact_tv_phoneNumber);
+
+                        String firstName = et_firstName.getText().toString();
+                        String lastName = et_lastName.getText().toString();
+                        String phoneNumber = et_phoneNumber.getText().toString();
+
+                        boolean isValid =
+                                (firstName.replace(" ", "").length() > 0 )
+                                        && (lastName.replace(" ", "").length() > 0 )
+                                        && (phoneNumber.replace(" ", "").length() > 0 );
+
+                        if(isValid) {
+                            Contact contact = new Contact(firstName, lastName, phoneNumber);
+                            ContactSdk.getInstance().addContact(contact, (ContactListAdapater)listView.getAdapter());
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Invalid input", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialogInterface) {
+                        EditText et_firstName = addContactView.findViewById(R.id.add_Contact_tv_firstName);
+                        EditText et_lastName = addContactView.findViewById(R.id.add_Contact_tv_lastName);
+                        EditText et_phoneNumber = addContactView.findViewById(R.id.add_Contact_tv_phoneNumber);
+
+                        et_firstName.setText("");
+                        et_lastName.setText("");
+                        et_phoneNumber.setText("");
+                    }
+                })
+                .setView(addContactView).create();
+
+        if(addContactView.getParent()!=null)
+            ((ViewGroup)addContactView.getParent()).removeView(addContactView);
+
+        addContactDialog.show();
     }
 
     /*** TESTS ***/
